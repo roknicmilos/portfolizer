345738e3f3ae5f2d96f03769399c1a79c32531a9
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-19 10:14:15 +0200

    Add missing tests

--- portfolizer/views.py
+++ portfolizer/views.py
@@ -1,18 +1,10 @@
-from dataclasses import dataclass
-
 from django.urls import reverse
 from django.views.generic import TemplateView
 from django.utils.translation import gettext_lazy as _
 
 from apps.common.utils import get_model_admin_change_list_url
+from apps.common.views import ButtonLink
 from apps.portfolio.models import Portfolio
-
-
-# TODO: move to common app
-@dataclass
-class ButtonLink:
-    label: str
-    url: str
 
 
 class IndexView(TemplateView):
@@ -26,9 +18,7 @@ class IndexView(TemplateView):
 
     def _create_portfolio_button(self) -> ButtonLink:
         if portfolio := self.request.user.portfolio:
-            url = reverse(
-                viewname="portfolio:index", kwargs={"slug": portfolio.slug}
-            )
+            url = reverse("portfolio:index", kwargs={"slug": portfolio.slug})
             return ButtonLink(label=_("My Portfolio"), url=url)
 
         url = get_model_admin_change_list_url(model_class=Portfolio)
