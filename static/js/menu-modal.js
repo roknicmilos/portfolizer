1a91f634f9d3c389206a1b593f21e61da3675352
Miloš Roknić <roknic.milos.994@gmail.com>
2024-11-30 23:45:05 +0100

    Cleanup menu-modal.js

--- static/js/menu-modal.js
+++ static/js/menu-modal.js
@@ -9,14 +9,12 @@ function prepareLanguageModal() {
     // Activate the menu when the burger is clicked:
     mainMenuBurger.addEventListener("click", () => {
         mainMenu.classList.add("menu__right--active");
-        console.log("OPEN MENU");
     });
 
     // Deactivate the menu when the menu background is clicked:
     mainMenu.addEventListener("click", (event) => {
-        // TODO: Prevent closing the menu if a child element inside the menu is clicked
+        // Prevent closing the menu if a child element inside the menu is clicked
         if (event.target !== mainMenu) return;
         mainMenu.classList.remove("menu__right--active");
-        console.log("CLOSE MENU");
     });
 }
