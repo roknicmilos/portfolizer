1fcf59549fa5d562f13b26173a78f380e5f0e956
Miloš Roknić <roknic.milos.994@gmail.com>
2024-12-01 13:40:30 +0100

    Add close button for the main menu

--- static/js/menu-modal.js
+++ static/js/menu-modal.js
@@ -5,16 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
 function prepareLanguageModal() {
     const mainMenu = document.querySelector(".js-main-menu");
     const mainMenuBurger = document.querySelector(".js-main-menu-burger");
+    const closeMainMenuButton = document.querySelector(".js-close-main-menu-button");
 
-    // Activate the menu when the burger is clicked:
+    // Open the menu when the burger is clicked:
     mainMenuBurger.addEventListener("click", () => {
         mainMenu.classList.add("menu__main--opened");
     });
 
-    // Deactivate the menu when the menu background is clicked:
+    // Close the menu when the menu background is clicked:
     mainMenu.addEventListener("click", (event) => {
         // Prevent closing the menu if a child element inside the menu is clicked
         if (event.target !== mainMenu) return;
+        mainMenu.classList.remove("menu__main--opened");
+    });
+
+    // Close the menu when the close button is clicked:
+    closeMainMenuButton.addEventListener("click", () => {
         mainMenu.classList.remove("menu__main--opened");
     });
 }
