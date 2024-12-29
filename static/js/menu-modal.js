40ea07c32c66518668aa0781406e8f97b01ddbe2
Miloš Roknić <roknic.milos.994@gmail.com>
2024-12-29 12:30:35 +0100

    Fix menu-modal.js

--- static/js/menu-modal.js
+++ static/js/menu-modal.js
@@ -7,12 +7,14 @@ function prepareLanguageModal() {
     const mainMenuBurger = document.querySelector(".js-main-menu-burger");
     const closeMainMenuButton = document.querySelector(".js-close-main-menu-button");
     const cvBackground = document.querySelector(".js-cv-background");
-    const cvBackgroundParent = cvBackground.parentElement;
+    const cvBackgroundParent = cvBackground ? cvBackground.parentElement : null;
 
     // Open the menu when the burger is clicked:
     mainMenuBurger.addEventListener("click", () => {
         mainMenu.classList.add("menu__main--opened");
-        cvBackground.remove();
+        if (cvBackground) {
+            cvBackground.remove();
+        }
     });
 
     // Close the menu when the menu background is clicked:
@@ -20,12 +22,16 @@ function prepareLanguageModal() {
         // Prevent closing the menu if a child element inside the menu is clicked
         if (event.target !== mainMenu) return;
         mainMenu.classList.remove("menu__main--opened");
-        cvBackgroundParent.appendChild(cvBackground);
+        if (cvBackgroundParent) {
+            cvBackgroundParent.appendChild(cvBackground);
+        }
     });
 
     // Close the menu when the close button is clicked:
     closeMainMenuButton.addEventListener("click", () => {
         mainMenu.classList.remove("menu__main--opened");
-        cvBackgroundParent.appendChild(cvBackground);
+        if (cvBackgroundParent) {
+            cvBackgroundParent.appendChild(cvBackground);
+        }
     });
 }
